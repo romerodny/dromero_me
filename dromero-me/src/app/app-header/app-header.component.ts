a9f02363455e6508bc98387c2bfc90696f115d0d
romerodny <romerodny@gmail.com>
2018-07-18 21:51:46 -0700

    More changes to the page. A lot of temporary business

---
+++ dromero-me/src/app/app-header/app-header.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-header',
+    templateUrl: './app-header.component.html'
+})
+export class HeaderComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit(): void { }
+}
--- dromero-me/src/app/app.module.ts
+++ dromero-me/src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HeaderComponent } from './app-header/app-header.component';
 import { AppComponent } from './app.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatCardModule,
+    MatToolbarModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
